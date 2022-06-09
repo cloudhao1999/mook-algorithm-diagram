27b35a0c34ed748473128dcbad45826e12d39faa
chen.yunhao <chenyunhao@sihesci-tech.com>
2022-06-09 14:26:40 +0800

    二分查找

---
+++ binary_search.ts
@@ -0,0 +1,21 @@
+function binarySearch(list: number[], item: number) {
+    let low = 0;
+    let high = list.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let guess = list[mid];
+
+        if (guess === item) {
+            return mid;
+        } else if (guess > item) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return null;
+}
+
+let myList = [1,3,5,7,9]
+console.log(binarySearch(myList,3))
