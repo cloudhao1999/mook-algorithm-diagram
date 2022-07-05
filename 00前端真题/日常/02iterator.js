77d2568cd7db4051af0b74a66e215687e150ea9b
cloudhao1999 <cyh19991002@gmail.com>
2022-07-05 16:10:21 +0800

    02iterator

---
+++ 00前端真题/日常/02iterator.js
@@ -0,0 +1,21 @@
+let range = {
+    from: 1,
+    to: 5,
+
+    [Symbol.iterator]() {
+        this.current = this.from
+        return this
+    },
+
+    next() {
+        if (this.current <= this.to) {
+            return { done: false, value: this.current++ }
+        } else {
+            return { done: true }
+        }
+    }
+}
+
+for (let num of range) {
+    console.log('num',num)
+}
